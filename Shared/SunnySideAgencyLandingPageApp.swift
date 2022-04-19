1bedd616562a570a1a4fa56fc49ed951dab343ac
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-19 11:50:50 +0900

    Initial Commit

---
+++ Shared/SunnySideAgencyLandingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunnySideAgencyLandingPageApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/19.
+//
+
+import SwiftUI
+
+@main
+struct SunnySideAgencyLandingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
